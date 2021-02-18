f1d2239957606082d0f82d15b092b3d440049c7e
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-18 22:29:15 +0500

    add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
-  
+
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
