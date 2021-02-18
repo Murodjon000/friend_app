9cf41b0d56274ed7a5cb2f3a9ffcec43046c4176
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-18 15:42:59 +0500

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all 
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.ordered_by_most_recent
+    @mutual_friends = @user.mutual_friends
+    @pending_friends = @user.pending
+  end
+end
