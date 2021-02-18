7fe75f23c641d68ce12373bd0798bb5d1ea13730
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-18 15:43:32 +0500

    add likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,28 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @like = current_user.likes.new(post_id: params[:post_id])
+
+    if @like.save
+      flash[:success] = 'You liked the post'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'You cannot like this post'
+      redirect_to posts_path
+    end
+  end
+
+  def destroy
+    like = Like.find_by(post_id: params[:post_id], user: current_user, id: params[:id])
+
+    if like
+        like.destroy
+        flash[:success] = 'You disliked the post'
+        redirect_to posts_path
+    else
+        flash[:alert] = 'You cannot dislike this post'
+        redirect_to posts_path
+    end
+  end
+end
