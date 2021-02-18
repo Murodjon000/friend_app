7113ef468e1c356cdbd76bca0225574670af09e5
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-18 15:43:58 +0500

    add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:success] = 'You commented to the post'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Comment was not created'
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
