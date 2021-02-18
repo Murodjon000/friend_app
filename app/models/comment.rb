f77ee6af0c89538b1b04326dcb81d86f35f4fa73
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-18 15:41:40 +0500

    add comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
+#
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :content, presence: true, length: { maximum: 200,
+                                                message: '200 characters in comment is the maximum allowed.' }
+end
