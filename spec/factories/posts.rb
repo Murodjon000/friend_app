7ec15c8769a785c4c9867f568f4ace73b9471949
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-19 20:38:53 +0500

    add posts factory

---
+++ spec/factories/posts.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+FactoryBot.define do
+  factory :post do
+    user
+    content { "This is post" }
+  end
+end
