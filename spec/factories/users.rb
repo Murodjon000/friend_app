cfc3a1c6eb6d807d3bb5cebe55c3cbfdeb0b9b8f
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-19 20:39:02 +0500

    add users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  gravatar_url           :string
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+FactoryBot.define do
+  factory :user, aliases: %i[receiver requester] do
+    name { 'John' }
+    sequence(:email) { |n| "john@ex#{n}.com" }
+    password { 'password' }
+  end
+end
