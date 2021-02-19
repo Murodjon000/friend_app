36eb71c221203f9eb8a915e6d94cc814583371ec
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-19 23:11:32 +0500

    add changes in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def users
+    @users = User.all
+    @user = User.find_by(params[:user_id])
+
+    if current_user
+      @users.reject { |user| user == current_user }
+    end
+  end
+
+
   before_action :permitted_params, if: :devise_controller?
   
   protected
