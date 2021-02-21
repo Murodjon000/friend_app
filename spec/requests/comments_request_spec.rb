651396b0844d5ac371f0da7f4bd3e4eebb193509
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-21 23:00:15 +0500

    add comments request spec

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  describe '#creat' do
+      let(:user) { create(:user) }
+      let(:content) { create(:post,user: user) }
+      
+      before { sign_in(:user) }
 
+      it 'should create comment if the request is valid' do
+          post post_comments_path(content), params: { comment: { content: 'this is comment' } }
+          expect(content.comments.count).to eq(1)
+      end
+      
+      it 'should not create comment if the request is invalid' do
+        post post_comments_path(content), params: { comment: { content: '' } }
+        expect(content.comments.count).to be_zero
+    end
+  end
 end
